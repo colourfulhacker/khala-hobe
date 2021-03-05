3eb036912cb72e6d835f5b289d2d300401d2a19f
Jit Banerjee <52858580+cehpoint@users.noreply.github.com>
2021-03-05 21:09:43 +0530

    Update Hackkorekhala.py

--- Hackkorekhala.py
+++ Hackkorekhala.py
@@ -1,11 +1,11 @@
 import base64
 import subprocess
-subprocess.call("curl -XPOST https://www.hackthebox.eu/api/invite/generate", shell=True) #The command to get the encypted invite-code
+subprocess.call("curl -XPOST https://www.hackthebox.eu/api/invite/generate", shell=True)
 print('\n')
-api = str(input("see the line =, copy it any paste here:\n==> ")) #To store the encrypted in a value
+api = str(input("see the line =, copy it any paste here:\n==> ")) 
 print('\n') #This steing will produce a new line
-dec = str(base64.standard_b64decode(api)) #The Decrypted
-print("[+]==> " + dec.replace('b', '')) #replacing th extra charater with a space
+dec = str(base64.standard_b64decode(api)) 
+print("[+]==> " + dec.replace('b', ''))
 print('\n')
-print("The format of the code is AAAA-BBBB-CCCC-DDDD-EEEE paste it in webiste") #This is The format of the code
+print("The format of the code is AAAA-BBBB-CCCC-DDDD-EEEE paste it in webiste") 
 print('\n')
