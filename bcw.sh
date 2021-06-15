d0fde99854d9278fe4b714bc6f3a0d0ba6ddc424
Jit Banerjee <52858580+cehpoint@users.noreply.github.com>
2021-06-16 01:04:25 +0530

    Create bcw.sh

---
+++ bcw.sh
@@ -0,0 +1,1 @@
+for i in {1..5}; do curl -s -L -i https://elearning.cehpoint.co.in/?author=$i | grep -E -o "\" title=\"View all posts by [a-z0-9A-Z\-\.]*|Location:.*" | sed 's/\// /g' | cut -f 6 -d ' ' | grep -v "^$"; done
